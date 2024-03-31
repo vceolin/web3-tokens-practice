54f4eed62e16b0e92ec30f4f40301e58ac324825
Vitor Ceolin <39313422+vceolin@users.noreply.github.com>
2024-03-31 18:11:02 -0300

    feat: also deploy NFT

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,13 +3,8 @@ const { ethers } = require('hardhat')
 async function main() {
   const [deployer] = await ethers.getSigners()
 
-  console.log('Deploying contracts with the account:', deployer.address)
-
-  const Skylean = await ethers.getContractFactory('Skylean')
-  const skylean = await Skylean.deploy()
-  const skyleanAdress = await skylean.getAddress()
-
-  console.log('Skylean deployed to:', skyleanAdress)
+  await deployERC20(deployer)
+  await deployERC721(deployer)
 }
 
 main()
@@ -18,3 +13,23 @@ main()
     console.error(error)
     process.exit(1)
   })
+
+async function deployERC20(deployer) {
+  console.log('Deploying ERC20 contracts with the account:', deployer.address)
+
+  const Skylean = await ethers.getContractFactory('Skylean')
+  const deployed = await Skylean.deploy()
+  const skyleanAdress = await deployed.getAddress()
+
+  console.log('Skylean deployed to:', skyleanAdress)
+}
+
+async function deployERC721(deployer) {
+  console.log('Deploying ERC721 contract with the account:', deployer.address)
+
+  const SkyleanNft = await ethers.getContractFactory('SkyleanNFT')
+  const deployed = await SkyleanNft.deploy()
+  const skyleanNftAdress = await deployed.getAddress()
+
+  console.log('ERC721 contract deployed to address:', skyleanNftAdress)
+}
